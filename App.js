e54be1797572253f67560da8e66c348c24262ee1
ranierishi <ranieri.shi@gmail.com>
2021-11-23 22:01:18 -0300

    visible

--- App.js
+++ App.js
@@ -9,15 +9,14 @@ import Botao from './botao/index'
 export default function App() {
   const [texto, setTexto] = useState('')
   const [textoTela, setTextoTela] =useState('')
+  const [visivel, setVisivel] = useState(false)
   
   return (
     <View style={styles.container}>
       <TextInput 
         onChangeText={(event) => setTexto(event)}
         style={{borderWidth:1, width: 100, paddingHorizontal:4}}
-      />
-      
-      
+      />     
       <TouchableOpacity
         onPress={()=> setTextoTela(texto)}
         style={{borderWidth:1, width: 100, paddingHorizontal:4, textAlign:'center', marginTop:4}}
@@ -26,7 +25,15 @@ export default function App() {
       </TouchableOpacity>
 
       <Text>{textoTela}</Text>
-    
+      
+      <TouchableOpacity
+        onPress={()=> setVisivel(oldState => !oldState)}
+        style={{borderWidth:1, width: 100, paddingHorizontal:4, textAlign:'center', marginTop:4}}
+      >
+        <Text>Botão Mostrar Texto</Text>
+      </TouchableOpacity>
+
+      { visivel && <Text>Olá</Text>}
     <StatusBar style="auto" />
     </View>
   );
@@ -46,9 +53,17 @@ const styles = StyleSheet.create({
 
 
 /*
+&&
+V V = V
+V F = F
+F V = F
+F F = F
 
-
-
+||
+V V = V
+V F = V
+F V = V
+F F = F
 
 
 */
